bdbd5d571bac93102ca937999da11e94aa1cb478
MidasNG <ngmidas09@gmail.com>
2025-01-07 10:18:50 +0100

    Removed extra cubemaps

---
+++ Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour
+{
+    private bool isHard;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
